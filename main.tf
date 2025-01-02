d3c25458649138b613a183e842c35784f3992800
Candido <candidocorporan144@gmail.com>
2025-01-01 20:56:13 -0500

    added terraform code for VPC

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
- I need a Job
+provider "aws" {
+  version = "~> 5.0"
+  profile = "default"
+  region  = "us-east-1"
+}
+terraform {
+  backend "s3" {
+    bucket = "terraformhw125"
+    key    = "devops/class/tfstate"
+    region = "us-east-1"
+  }
+}
+resource "aws_vpc" "main" {
+  cidr_block          = "12.0.0.0/16"
+  instance_tenancy      = "default"
+  tags = {
+    Name = "Vpctosendtobitbucket"
+  }
+}
