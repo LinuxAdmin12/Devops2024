a0b8350b5cf2aa568f4a895f5a0598d59c3b8402
Candido <candidocorporan144@gmail.com>
2025-01-03 14:43:43 -0500

    added terraform code to create VPC with 2 sub net and route tables and IGW

--- main.tf
+++ main.tf
@@ -1,19 +1,56 @@
 provider "aws" {
-  version = "~> 5.0"
-  profile = "default"
-  region  = "us-east-1"
+  region = "us-east-1" 
 }
-terraform {
-  backend "s3" {
-    bucket = "terraformhw125"
-    key    = "devops/class/tfstate"
-    region = "us-east-1"
-  }
-}
-resource "aws_vpc" "main" {
-  cidr_block          = "12.0.0.0/16"
-  instance_tenancy      = "default"
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true          
+  enable_dns_hostnames = true         
   tags = {
-    Name = "Vpctosendtobitbucket"
+    Name = "my-vpcnextlevel"
   }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24" 
+  availability_zone = "us-east-1a" 
+  map_public_ip_on_launch = true  
+  tags = {
+    Name = "public-subnet1"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.2.0/24" 
+  availability_zone = "us-east-1b" 
+  map_public_ip_on_launch = false 
+  tags = {
+    Name = "private-subnet1"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "my-igwNL"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0" 
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
 }
